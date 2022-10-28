81b3dd1e2437b651049e6b3d7bbff22b24b69dc0
Alejandro Vidal Perez <alevidalperez88@gmail.com>
2022-10-28 12:30:36 -0500

    Add documentation

--- src/main.java
+++ src/main.java
@@ -1,6 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+/*
+8.10
+Utilizar una lista doblemente enlazada para controlar una lista de pasajeros de una linea aérea. El programa
+principal debe ser controlado por men+u y permitir al usuario visualizar los datos de un pasajero determinado,
+insertar un nodo (siempre por el final) y eliminar un pasajero de la lista. A la lista se accede por 2 variables,
+una referencia al primer nodo y la otra al último nodo.
+ */
+
 
 
 
@@ -14,6 +22,7 @@ class Autobus_1 {
         String dato;
         boolean si = true;
 
+        //Insertar pasajeros
         ld.insertarFin("Elmer");
         ld.insertarFin("Enrique");
         ld.insertarFin("Alejandro");
@@ -21,6 +30,7 @@ class Autobus_1 {
 
 
 
+        //Desplegar Menú
         while(si)
         {
             System.out.println("------- AEROLINEA ---------");
@@ -35,6 +45,7 @@ class Autobus_1 {
 
             switch(opcion)
             {
+                //Opción1. Agregar pasajero
                 case 1:  System.out.println("Agrega un pasajero");
                     dato = (entrada.readLine());
                     ld.insertarFin(dato);
@@ -42,6 +53,7 @@ class Autobus_1 {
                     ld.Mostrar();
                     break;
 
+                    //Opcion2. Eliminar pasajero
                 case 2: System.out.println("¿Qué pasajero quiere eliminar?");
                     dato = (entrada.readLine());
                     ld.eliminaDato(dato);
@@ -49,11 +61,13 @@ class Autobus_1 {
                     ld.Mostrar();
                     break;
 
+                    //Opcion 3. Ver datos del pasajero
                 case 3: System.out.println("Nombre del pasajero");
                     dato = (entrada.readLine());
                     System.out.print(ld.buscar(dato));
                     break;
 
+                    //Mandar el mensaje si no se eligen las opciones del menú
                 default: System.out.println("Lo siento, no entendí, vuelva más tarde.");
                     si = false;
                     break;
